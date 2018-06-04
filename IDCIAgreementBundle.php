676ca17c81a4d958ebdb080bcccdccc7c0b74954
Eluminae <eddie.barraco@idci-consulting.fr>
2018-06-04 14:50:49 +0200

    Initial commit.

---
+++ IDCIAgreementBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace IDCI\Bundle\AgreementBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class IDCIAgreementBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
+}
